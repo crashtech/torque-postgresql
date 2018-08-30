7576e848c393280112e3db48f6b8eaeb548fbe7a
Carlos Silva <carlinhus.fsilva@gmail.com>
2018-08-30 13:58:48 -0300

    Fixes for production deploy

--- lib/torque/postgresql/railtie.rb
+++ lib/torque/postgresql/railtie.rb
@@ -3,11 +3,8 @@ module Torque
     # = Torque PostgreSQL Railtie
     class Railtie < Rails::Railtie # :nodoc:
 
-      # Eger load PostgreSQL namespace
-      config.eager_load_namespaces << Torque::PostgreSQL
-
       # Get information from the running rails app
-      runner do |app|
+      initializer 'torque-postgresql' do |app|
         Torque::PostgreSQL.config.eager_load = app.config.eager_load
       end
 
