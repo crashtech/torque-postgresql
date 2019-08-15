fea08a556022b2d72c6ad2440dc1113562448a54
Carlos Silva <carlinhus.fsilva@gmail.com>
2019-08-15 00:13:50 -0300

    Fix time keeper model

--- spec/models/time_keeper.rb
+++ spec/models/time_keeper.rb
@@ -1,9 +1,9 @@
 class TimeKeeper < ActiveRecord::Base
-  period_for :available, pessimistic: true, methods: {
-    current:     :available,
-    not_current: :unavailable,
+  # period_for :available, pessimistic: true, methods: {
+  #   current:     :available,
+  #   not_current: :unavailable,
 
-    current?:    :available?,
-    current_on?: :available_on?,
-  }
+  #   current?:    :available?,
+  #   current_on?: :available_on?,
+  # }
 end
