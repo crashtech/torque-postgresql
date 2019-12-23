f89eff70ee2736c3728244168beaac0221014935
Carlos Silva <carlinhus.fsilva@gmail.com>
2019-12-23 13:21:29 -0300

    Fix specs

--- spec/schema.rb
+++ spec/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 begin
-  version = 47
+  version = 49
 
   raise SystemExit if ActiveRecord::Migrator.current_version == version
   ActiveRecord::Schema.define(version: version) do
